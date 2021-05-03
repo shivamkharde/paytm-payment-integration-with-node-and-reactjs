e9f4291fafd9dfb0e1fe4fec1832d46255b4b4bf
shivamkharde <jarvis.shivam@gmail.com>
2021-05-03 18:36:25 +0530

    done with paytm integration

--- paytm-frontend/src/pages/home/index.js
+++ paytm-frontend/src/pages/home/index.js
@@ -3,31 +3,71 @@ import { PaytmPaymentBtn } from "../../components";
 import "./style.css";
 
 export const Home = () => {
-    const createFormAndSubmit = (data) => {
-        // create form and submit it
-        const formElement = document.createElement("form");
-        formElement.setAttribute("name", "paytm-form");
-        formElement.setAttribute("method", "POST");
-        formElement.setAttribute("action", data.txn_url);
-    };
+	function isDate(val) {
+		// Cross realm comptatible
+		return Object.prototype.toString.call(val) === "[object Date]";
+	}
 
-    const startPayment = () => {
-        // call an api for payment
-        fetch("http://localhost:5500/paynow", {
-            method: "POST",
-        })
-            .then((data) => {
-                console.log(data);
-            })
-            .catch((err) => {
-                console.log(
-                    "something went wrong while initiating the payment ! please try again",
-                );
-            });
-    };
-    return (
-        <div className="container">
-            <PaytmPaymentBtn onclick={startPayment} />
-        </div>
-    );
+	function isObj(val) {
+		return typeof val === "object";
+	}
+
+	function stringifyValue(val) {
+		if (isObj(val) && !isDate(val)) {
+			return JSON.stringify(val);
+		} else {
+			return val;
+		}
+	}
+	function buildForm({ action, params }) {
+		const form = document.createElement("form");
+		form.setAttribute("method", "post");
+		form.setAttribute("action", action);
+
+		Object.keys(params).forEach((key) => {
+			const input = document.createElement("input");
+			input.setAttribute("type", "hidden");
+			input.setAttribute("name", key);
+			input.setAttribute("value", stringifyValue(params[key]));
+			form.appendChild(input);
+		});
+
+		return form;
+	}
+
+	function createFormAndSubmit(details) {
+		const form = buildForm(details);
+		document.body.appendChild(form);
+		form.submit();
+		form.remove();
+	}
+
+	const startPayment = () => {
+		// call an api for payment
+		fetch("http://localhost:5500/payment/paynow", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+		})
+			.then((data) => {
+				return data.json();
+			})
+			.then((data) => {
+				let information = {
+					action: "https://securegw-stage.paytm.in/order/process",
+					params: data,
+				};
+				createFormAndSubmit(information);
+			})
+			.catch((err) => {
+				console.log("something went wrong while initiating the payment ! please try again");
+			});
+	};
+	return (
+		<div className="container">
+			<PaytmPaymentBtn onclick={startPayment} />
+		</div>
+	);
 };
